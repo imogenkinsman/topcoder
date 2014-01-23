f67c4ebce90bcf86fa5fae78318e276052272fe3
Jade McGough <jade@thezets.com>
2014-01-23 15:43:33 -0800

    zigzag

--- lib/zigzag.rb
+++ lib/zigzag.rb
@@ -1,1 +1,27 @@
 #TopCoder Collegiate Challenge Semifinal Round 3 - Divison 1, Level One
+
+def zigzag(sequence)
+
+  length = []
+  change = [] # was last change in sequence positive? :negative, :positive
+
+  sequence.each_with_index do |number, index|
+    length[index] = max_length = 1
+    0.upto(index - 1) do |prior_index|
+      prior_number = sequence[prior_index]
+      prior_length = length[prior_index]
+
+      if prior_length >= max_length
+        if prior_number < number && change[prior_index] != :positive
+          change[index] = :positive
+          max_length = prior_length + 1
+        elsif prior_number > number && change[prior_index] != :negative
+          change[index] = :negative
+          max_length = prior_length + 1
+        end
+      end
+    end
+    length[index] = max_length
+  end
+  return length.max
+end
--- spec/zigzag_spec.rb
+++ spec/zigzag_spec.rb
@@ -3,12 +3,15 @@ require_relative '../lib/zigzag'
 describe 'zig zag' do
 
   it 'returns the correct length' do
-    expect(zigzag[1, 7, 4, 9, 2, 5]).to eq 6
-    expect(zigzag[1, 17, 5, 10, 13, 15, 10, 5, 16, 8]).to eq 7
-    expect(zigzag[44]).to eq 1
-    expect(zigzag[70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32]).to eq 8
+    expect(zigzag [1, 7, 4, 9, 2, 5]).to eq 6
+    expect(zigzag [1, 17, 5, 10, 13, 15, 10, 5, 16, 8]).to eq 7
+    expect(zigzag [44]).to eq 1
+    expect(zigzag [70, 55, 13, 2, 99, 2, 80, 80, 80, 80, 100, 19, 7, 5, 5, 5, 1000, 32, 32]).to eq 8
+    expect(zigzag [374, 40, 854, 203, 203, 156, 362, 279, 812, 955,
+                  600, 947, 978, 46, 100, 953, 670, 862, 568, 188,
+                  67, 669, 810, 704, 52, 861, 49, 640, 370, 908,
+                  477, 245, 413, 109, 659, 401, 483, 308, 609, 120,
+                  249, 22, 176, 279, 23, 22, 617, 462, 459, 244]).to eq 36
   end
-
-
 
 end
