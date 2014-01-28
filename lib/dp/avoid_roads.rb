8694a23d5cf6bb918e1b6d93c26d16a5d503d333
Jade McGough <jade@thezets.com>
2014-01-28 04:34:39 -0800

    avoid roads

--- lib/dp/avoid_roads.rb
+++ lib/dp/avoid_roads.rb
@@ -10,21 +10,25 @@ end
 # this might be cleaner with recursion
 def avoid_roads(width, height, bad)
 
-  routes = Array.new(height + 1, Array.new(width + 1, nil)) # how many routes to a block?
+  routes = Array.new(height + 1) { Array.new(width + 1) { 0 } } # how many routes to a block?
 
   routes.each_with_index do |row, i|
     row.each_index do |j|
-      if i == 0 && j == 0
-
+      if (i == 0 && j == 0)
+        routes[0][0] = 1
       elsif i == 0
-
+        routes[i][j] = routes[0][j - 1] unless is_obstructed?(i, j, i, j - 1, bad)
       elsif j == 0
+        routes[i][j] = routes[i - 1][0] unless is_obstructed?(i, j, i - 1, j, bad)
       else
-
+        top = (is_obstructed?(i, j, i - 1, j, bad) ? 0 : routes[i - 1][j])
+        left = (is_obstructed?(i, j, i, j - 1, bad) ? 0 : routes[i][j - 1])
+        routes[i][j] = top + left
       end
-
     end
   end
 
   return routes[height][width]
 end
+
+avoid_roads(6, 6, ["0 0 0 1", "6 6 5 6"])
